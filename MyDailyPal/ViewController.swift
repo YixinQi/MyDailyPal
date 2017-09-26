a11c11793373ff5ccd80e765fcff899e607763da
Yixin <yixin.qi08@gmail.com>
2017-09-26 11:54:21 -0400

    Initial Commit

---
+++ MyDailyPal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDailyPal
+//
+//  Created by Yixin Qi on 9/26/17.
+//  Copyright © 2017 Yixin Qi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
