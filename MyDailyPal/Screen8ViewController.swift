10c47130659975d9f8c2fc1632131c5c7c50ff59
ROD Shangari <rodshangari@mankarans-air.wv.cc.cmu.edu>
2017-10-08 22:54:44 -0400

    Screen 8 and & view controller and views with navigation controls, just the UI element

---
+++ MyDailyPal/Screen8ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Screen8ViewController.swift
+//  MyDailyPal
+//
+//  Created by ROD Shangari on 10/8/17.
+//  Copyright © 2017 Yixin Qi. All rights reserved.
+//
+
+import UIKit
+
+class Screen8ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
