fe5886ab60c7e5dde4d358c5bbd9f59f529e39e0
Yixin <yixin.qi08@gmail.com>
2017-11-22 22:54:39 -0500

    Added JTAppleCalendar framework

---
+++ MyDailyPal/Controller/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  MyDailyPal
+//
+//  Created by Yixin Qi on 11/21/17.
+//  Copyright © 2017 Yixin Qi. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+  @IBOutlet weak var dateLabel: UILabel!
+}
