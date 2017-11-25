5b80492d6d483c8cf3d09e98fc7c39a7dba8f38a
Yixin <yixin.qi08@gmail.com>
2017-11-25 14:08:23 -0500

    Adjusted screen 10.

--- MyDailyPal/Controller/Screen10ViewController.swift
+++ MyDailyPal/Controller/Screen10ViewController.swift
@@ -12,10 +12,19 @@ class Screen10ViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
     return 1
   }
+
+  @IBOutlet weak var Monday: UILabel!
+  @IBOutlet weak var Tuesday: UILabel!
+  @IBOutlet weak var Wendsday: UILabel!
+  @IBOutlet weak var Thursday: UILabel!
+  @IBOutlet weak var Friday: UILabel!
+  @IBOutlet weak var Saturday: UILabel!
+  @IBOutlet weak var Sunday: UILabel!
   
   @IBOutlet weak var Medication: UILabel!
   @IBOutlet weak var timePicker: UIDatePicker!
   @IBOutlet weak var transparentBackgound: UIView!
+  @IBOutlet weak var datePicker: UIDatePicker!
   @IBOutlet weak var timePickerBackground: UIView!
   @IBOutlet weak var setTimeLabel: UILabel!
   @IBOutlet weak var saveTimeButton: UIButton!
@@ -41,6 +50,28 @@ class Screen10ViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         let tap = UITapGestureRecognizer(target: self, action: #selector(tap(gestureRecognizer:)))
         setTimeLabel.addGestureRecognizer(tap)
         setTimeLabel.isUserInteractionEnabled = true
+        let tap1 = UITapGestureRecognizer(target: self, action: #selector(tap1(gestureRecognizer:)))
+        Monday.addGestureRecognizer(tap1)
+        Monday.isUserInteractionEnabled = true
+        let tap2 = UITapGestureRecognizer(target: self, action: #selector(tap2(gestureRecognizer:)))
+        Tuesday.addGestureRecognizer(tap2)
+        Tuesday.isUserInteractionEnabled = true
+        let tap3 = UITapGestureRecognizer(target: self, action: #selector(tap3(gestureRecognizer:)))
+        Wendsday.addGestureRecognizer(tap3)
+        Wendsday.isUserInteractionEnabled = true
+        let tap4 = UITapGestureRecognizer(target: self, action: #selector(tap4(gestureRecognizer:)))
+        Thursday.addGestureRecognizer(tap4)
+        Thursday.isUserInteractionEnabled = true
+        let tap5 = UITapGestureRecognizer(target: self, action: #selector(tap5(gestureRecognizer:)))
+        Friday.addGestureRecognizer(tap5)
+        Friday.isUserInteractionEnabled = true
+        let tap6 = UITapGestureRecognizer(target: self, action: #selector(tap6(gestureRecognizer:)))
+        Saturday.addGestureRecognizer(tap6)
+        Saturday.isUserInteractionEnabled = true
+        let tap7 = UITapGestureRecognizer(target: self, action: #selector(tap7(gestureRecognizer:)))
+        Sunday.addGestureRecognizer(tap7)
+        Sunday.isUserInteractionEnabled = true
+        treatmentPlan.repeate = ""
         // Do any additional setup after loading the view.
     }
 
@@ -49,7 +80,8 @@ class Screen10ViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     //treatmentPlan.attribute = "b"
     treatmentPlan.noOfDosage = Int16(dosagePicker.selectedRow(inComponent: 0))
     treatmentPlan.noOfTablet = Int16(tabletPicker.selectedRow(inComponent: 0))
-    treatmentPlan.startDate = timePicker.date as NSDate
+    treatmentPlan.startDate = datePicker.date as NSDate
+    //treatmentPlan.remindTime = timePicker.date as NSDate
     PersistenceService.saveContext()
     performSegue(withIdentifier: "tomytreatment", sender: self)
   }
@@ -82,6 +114,34 @@ class Screen10ViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     saveTimeButton.isHidden = false
     cancelTimeButton.isHidden = false
     //transparentBackground.isHidden = false
+  }
+  @objc func tap1(gestureRecognizer: UITapGestureRecognizer){
+    Monday.textColor = UIColor.red
+    treatmentPlan.repeate = treatmentPlan.repeate! + Monday.text!
+  }
+  @objc func tap2(gestureRecognizer: UITapGestureRecognizer){
+    Tuesday.textColor = UIColor.red
+    treatmentPlan.repeate = treatmentPlan.repeate! + Tuesday.text!
+  }
+  @objc func tap3(gestureRecognizer: UITapGestureRecognizer){
+    Wendsday.textColor = UIColor.red
+    treatmentPlan.repeate = treatmentPlan.repeate! + Wendsday.text!
+  }
+  @objc func tap4(gestureRecognizer: UITapGestureRecognizer){
+    Thursday.textColor = UIColor.red
+    treatmentPlan.repeate = treatmentPlan.repeate! + Thursday.text!
+  }
+  @objc func tap5(gestureRecognizer: UITapGestureRecognizer){
+    Friday.textColor = UIColor.red
+    treatmentPlan.repeate = treatmentPlan.repeate! + Friday.text!
+  }
+  @objc func tap6(gestureRecognizer: UITapGestureRecognizer){
+    Saturday.textColor = UIColor.red
+    treatmentPlan.repeate = treatmentPlan.repeate! + Saturday.text!
+  }
+  @objc func tap7(gestureRecognizer: UITapGestureRecognizer){
+    Sunday.textColor = UIColor.red
+    treatmentPlan.repeate = treatmentPlan.repeate! + Sunday.text!
   }
 
   @IBAction func clickSaveButton(_ sender: UIButton) {
