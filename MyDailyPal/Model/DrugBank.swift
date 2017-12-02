7fd2fbca32ded7526d834d3c70bda465006b0daf
ROD Shangari <rodshangari@mankarans-macbook-air.local>
2017-12-02 15:10:11 -0500

    Commit Drug bank locally

--- MyDailyPal/Model/DrugBank.swift
+++ MyDailyPal/Model/DrugBank.swift
@@ -21,8 +21,8 @@ class DrugBank {
         // Get contents in directory: '.' (current one)
         do {
         // tried relative path
-           
-            let files = try fileManager.contentsOfDirectory(atPath: "")
+            let path = Bundle.main.path(forResource: "assets", ofType: nil)
+            let files = try fileManager.contentsOfDirectory(atPath: path!)
             for file in files{
                 filename = file
                 drugList.append([filename])
