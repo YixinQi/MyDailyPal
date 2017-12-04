3bac493b87ed386120ca8e8076964987ff54923d
ROD Shangari <rodshangari@mankarans-macbook-air.local>
2017-12-04 00:41:32 -0500

    Screen 12 VC

--- MyDailyPal/Controller/Screen12TableViewController.swift
+++ MyDailyPal/Controller/Screen12TableViewController.swift
@@ -21,7 +21,6 @@ class Screen12TableViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         let formatter = DateFormatter()
-        
         //This block may be replaced if yixin passes you the actual array of adhrerenceRecords that you need. otherwise you would do this fetch request and loop through all the records and pull only the ones with the same date as the date you need.
         let adherenceRecordFetchRequest: NSFetchRequest<AdherenceRecord> = AdherenceRecord.fetchRequest()
         do {
@@ -29,14 +28,15 @@ class Screen12TableViewController: UIViewController, UITableViewDelegate, UITabl
             self.adherenceRecords = adherence
             
         } catch {}
-        
         if (adherenceRecords.count > 0){
+            
             for adhRecord in adherenceRecords{
                 if adhRecord.date == dateSelected{
-                    record.append(adhRecord)
+                    print("dates checked fine")
+                    self.record.append(adhRecord)
                 }
             }
-            date.text = "My Medicine history for "+formatter.string(from: record[0].date! as Date)
+            date.text = "My Medicine history for "+formatter.string(from: dateSelected as Date)
         }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -61,11 +61,13 @@ class Screen12TableViewController: UIViewController, UITableViewDelegate, UITabl
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        print("Number of records ",record.count)
         return record.count
     }
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("in table cell creation")
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Screen12TableViewCell", for: indexPath)
             as? Screen12TableViewCell else {
                 fatalError("The dequed cell is not an instance of Screen12TableViewController")
@@ -74,6 +76,7 @@ class Screen12TableViewController: UIViewController, UITableViewDelegate, UITabl
         // Configure the cell...
         let adherenceRecord = adherenceRecords[indexPath.row]
         cell.DrugName.text = adherenceRecord.treatmentName;
+        
         if adherenceRecord.didTake == true {
             roundImg.layer.cornerRadius = roundImg.frame.size.width/2
             roundImg.clipsToBounds = true
